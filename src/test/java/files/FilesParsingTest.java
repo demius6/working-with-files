765f07cb30b4f7d7ad029b9a897ee0d2bba2e9b3
demius6 <ya@gmail.com>
2024-03-24 21:58:18 +0300

    xls test v1

--- src/test/java/files/FilesParsingTest.java
+++ src/test/java/files/FilesParsingTest.java
@@ -1,2 +1,35 @@
-package files;public class FilesParsingTest {
+package files;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FilesParsingTest {
+
+    @Test
+    void parsePdfTest() throws Exception {
+        open("https://junit.org/junit5/docs/current/user-guide/");
+        File pdfDownload = $(byText("PDF download")).download();
+        PDF pdf = new PDF(pdfDownload);
+        assertThat(pdf.author).contains("Marc Philipp");
+        assertThat(pdf.numberOfPages).isEqualTo(179);
+    }
+
+    @Test
+    void parseXlsTest() throws Exception {
+        open("https://romashka2008.ru/price");
+        File xlsDownload = $(".site-main__inner a[href*='prajs_ot']").download();
+        XLS xls = new XLS(xlsDownload);
+        assertThat(xls.excel
+                .getSheetAt(0)
+                .getRow(11)
+                .getCell(1)
+                .getStringCellValue()).contains("Сахалинская обл. Южно-Сахалинск");
+    }
+
 }
